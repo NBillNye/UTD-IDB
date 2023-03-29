9787e5c410b628cc56abae4184f267389a2ec209
Ryan Talbot <ryan.talbot@evolvemortgageservices.com>
2023-03-29 01:25:45 -0500

    Start GetUserInformation and GetThreadList methods

--- Website/backend/views.py
+++ Website/backend/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 def index(request):
--- Website/frontend/views.py
+++ Website/frontend/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+import requests
+from decouple import config
 
 # Create your views here.
 
 def ThreadList(response):
+    print("REQUEST >>>> ", config("API_URL") + "threads/GetThreadList/1/")
+    threads = requests.post(config("API_URL") + "threads/GetThreadList/1/")
+    print("RESPONSE >>>> ", threads)
+    if threads is not None:
+        threads_data = threads.json()
+        print("threads_data >>>> ", threads_data)
+        return render(response, "ThreadList/index.html", {"threads": threads_data})
     return render(response, "ThreadList/index.html", {})
+        
