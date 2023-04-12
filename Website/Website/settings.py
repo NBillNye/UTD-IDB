df09424dcbf43909a724641cc98c1fd2691604d7
RTalbot1 <ry.l.talbot2000@gmail.com>
2023-04-11 23:23:03 -0500

    Change allowed hosts

--- Website/Website/settings.py
+++ Website/Website/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1','csa-4485-10.utdallas.edu']
 
 APPEND_SLASH=False
 
