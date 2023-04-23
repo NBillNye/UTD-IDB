9f8a20b0b8464d8cf7904cc649444f6c81a2db63
Nabil Habibi <nbill1235@gmail.com>
2023-04-23 18:33:02 -0500

    Process Thread Added

--- Website/frontend/MLModel/text_processing_util.py
+++ Website/frontend/MLModel/text_processing_util.py
@@ -175,13 +175,31 @@ def process_file(class_data_path, file_path, kw_model):
     process_course_material(class_data_path, file_path, kw_model)
         
 
-def process_thread(class_ID, thread_ID, kw_model):
+def process_thread(class_data_path, thread_ID, kw_model):
     threadtext = get_thread_and_reply_text(thread_ID)
     
-    class_data = pickle.load(threadtext) # list of processed Doc objs
- 
-    text = f.read()
-    keywords = keyword_extraction.extract_keywords(kw_model, text, ngram_range=3, top_n=10)
+    try:
+        with open(class_data_path, 'rb') as p:
+            class_data = pickle.load(p) # list of processed Doc objs
+    except FileNotFoundError:
+        class_data = []
+
+    keywords = keyword_extraction.extract_keywords(kw_model, threadtext, ngram_range=3, top_n=10)
+    
+    existing_doc_index = existing_doc(class_data, threadtext)
+    
+    if existing_doc_index != -1: # just update the keywords
+        class_data[existing_doc_index].keywords = keywords
+    else:
+        doc = Doc()
+        doc.set_text(threadtext)
+        doc.set_keywords(keywords)
+        doc.set_origin(thread_ID)
+        
+        class_data.append(doc)
+        
+    with open(class_data_path, 'wb') as new_p:
+        pickle.dump(class_data, new_p) # overwrite
 
 
 
@@ -196,9 +214,12 @@ def get_thread_and_reply_text(thread_ID):
     
     dbcursor = db.cursor()
 
-    dbcursor.execute("SELECT t.ThreadTitle, t.ThreadContent, r.Content FROM Thread as t, Reply as r WHERE t.ThreadID = (%s) AND t.ThreadID = r.Thread_ThreadID", (thread_ID))
+    dbcursor.execute("SELECT DISTINCT t.ThreadTitle, t.ThreadContent, r.Content FROM Thread as t, Reply as r WHERE t.ThreadID = (%s) AND t.ThreadID = r.Thread_ThreadID", (thread_ID))
 
-    result = dbcursor.fetchall()
+    result = dbcursor.fetchall() # Grab query result from cursor
+    
+    dbcursor.close()
+    
     text = ''
     for x in result:
         text += x
@@ -239,5 +260,7 @@ def del_document(class_ID: int, origin: int | str):
 
 if __name__ == '__main__':
 
-    new_process(12345, file_path = 'Syllabus-3377-converted.docx')
+    #new_process(12345, file_path = 'Syllabus-3377-converted.docx')
+    new_process(class_id = 12345, thread_ID=6)
+
     #del_document(12345, 'Syllabus-3377-converted.docx')
