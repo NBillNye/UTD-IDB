a7604653adfe9e4f14a5d115396bfe438c6276d4
Lia-Sar <sarahliang2001@gmail.com>
2023-04-28 11:26:02 -0500

    create login system

--- Website/frontend/models.py
+++ Website/frontend/models.py
@@ -69,13 +69,13 @@ class Reply(models.Model):
 
 
 class Student(models.Model):
-    permissions = models.IntegerField(db_column='Permissions')  # Field name made lowercase.
+    permissions = models.IntegerField(db_column='Permissions', null=True)  # Field name made lowercase.
     netid = models.CharField(db_column='NetID', primary_key=True, max_length=11)  # Field name made lowercase.
     firstname = models.CharField(db_column='FirstName', max_length=15)  # Field name made lowercase.
     lastname = models.CharField(db_column='LastName', max_length=15)  # Field name made lowercase.
-    email = models.CharField(max_length=45)
-    password = models.CharField(db_column='Password', max_length=18, blank=True, null=True)  # Field name made lowercase.
-
+    email = models.CharField(max_length=50)
+    password = models.CharField(db_column='Password', max_length=20, blank=True, null=True)  # Field name made lowercase.
+        
     class Meta:
         managed = False
         db_table = 'Student'
